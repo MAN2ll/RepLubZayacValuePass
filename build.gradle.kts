2cf2ac10c0d687b82380108e5268f088f3b165f8
MAN2ll <a.n.moiseev89@gmail.com>
2026-09-01 16:32:58 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // <-- Обновлено (было 1.9.20)
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false // <-- Обновлено до 2.48.1 для стабильности
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    // <-- Исправлено для совместимости с Gradle 8+
-    delete(rootProject.layout.buildDirectory) 
+    delete(rootProject.buildDir)
 }
